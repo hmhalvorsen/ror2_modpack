6fd4a7eb98981abd7e9c89dc81dc6c9f6ffc08f7
hmhalvorsen <henrik.halvorsen@nordicsemi.no>
2024-04-16 16:54:19 +0200

    directory: move all files to bin folder 2

--- update_modpack.sh
+++ update_modpack.sh
@@ -23,7 +23,7 @@ git clone https://github.com/hmhalvorsen/ror2_modpack /tmp/ror2_modpack
 
 # Kopier filer til Risk of Rain 2-mappen
 echo_and_log "Kopierer filer til Risk of Rain 2-mappen..."
-cp -r /tmp/ror2_modpack/* "/c/Program Files (x86)/Steam/steamapps/common/Risk of Rain 2/"
+cp -r /tmp/ror2_modpack/bin/* "/c/Program Files (x86)/Steam/steamapps/common/Risk of Rain 2/"
 
 # Logg slutten på skriptet
 echo_and_log "Skriptet fullført! Filer er hentet fra Putin."
